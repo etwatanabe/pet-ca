63f0d36262b070556737850f78773b22843aac99
Eduardo Takeshi <wattksh@gmail.com>
2025-05-22 15:00:37 -0300

    refactor: add http statuscode enum

--- src/controllers/protocols.ts
+++ src/controllers/protocols.ts
@@ -1,5 +1,5 @@
 export interface HttpResponse<T> {
-  statusCode: number;
+  statusCode: HttpStatusCode;
   body: T;
 }
 
@@ -7,6 +7,13 @@ export interface HttpRequest<B> {
   body?: B;
   headers?: any;
   params?: any;
+}
+
+export enum HttpStatusCode {
+  OK = 200,
+  CREATED = 201,
+  BAD_REQUEST = 400,
+  SERVER_ERROR = 500,
 }
 
 export interface IController {
