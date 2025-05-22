420893a3c67707770862e3cb35abc0c476246600
Eduardo Takeshi <wattksh@gmail.com>
2025-05-22 10:54:22 -0300

    feat: ensure GetUsersMongoRepository gets users from mongo

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,15 +1,17 @@
 import { IGetUserRepository } from "../../controllers/get-users/protocols";
+import { MongoClient } from "../../database/mongo";
 import { User } from "../../models/user";
 
 export class MongoGetUsersRepository implements IGetUserRepository {
   async getUsers(): Promise<User[]> {
-    return [
-      {
-        firstName: "John",
-        lastName: "Doe",
-        email: "johndoe@email.com",
-        password: "JohnDoe123",
-      },
-    ];
+    const users = await MongoClient.db
+      .collection<Omit<User, "id">>("users")
+      .find({})
+      .toArray();
+
+    return users.map(({ _id, ...user }) => ({
+      ...user,
+      id: _id.toHexString(),
+    }));
   }
 }
