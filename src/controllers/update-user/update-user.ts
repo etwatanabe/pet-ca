f955ef6d72fc484d25f790aa6977f30a40c6d0db
Eduardo Takeshi <wattksh@gmail.com>
2025-05-22 12:28:57 -0300

    feat: add UpdateUserController with validations

---
+++ src/controllers/update-user/update-user.ts
@@ -0,0 +1,57 @@
+import { User } from "../../models/user";
+import { HttpRequest, HttpResponse } from "../protocols";
+import {
+  IUpdateUserController,
+  IUpdateUserRepository,
+  UpdateUserParams,
+} from "./protocols";
+
+export class UpdateUserController implements IUpdateUserController {
+  constructor(private readonly updateUserRepository: IUpdateUserRepository) {}
+
+  async handle(
+    httpRequest: HttpRequest<any>
+  ): Promise<HttpResponse<User>> {
+    try {
+      const id = httpRequest?.params?.id;
+      const body = httpRequest?.body;
+
+      if (!id) {
+        return {
+          statusCode: 400,
+          body: "Missing id",
+        };
+      }
+
+      const allowedFieldsToUpdate: (keyof UpdateUserParams)[] = [
+        "firstName",
+        "lastName",
+        "email",
+        "password",
+      ];
+
+      const someFieldIsNotAllowedToUpdate = Object.keys(body).some(
+        (key) => !allowedFieldsToUpdate.includes(key as keyof UpdateUserParams)
+      );
+
+      if (someFieldIsNotAllowedToUpdate) {
+        return {
+          statusCode: 400,
+          body: "Some fields are not allowed to update",
+        };
+      }
+
+      const user = await this.updateUserRepository.updateUser(id, body);
+
+      return {
+        statusCode: 200,
+        body: user,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Internal server error",
+      };
+    }
+  }
+}
